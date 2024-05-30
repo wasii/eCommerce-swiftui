03a3c92f4527663ba56231a11df4937e50ec55b2
abdWasiq <khatri.wasiq@gmail.com>
2024-05-30 10:27:24 +0500

    project init

---
+++ ecommerce-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ecommerce-swiftui
+//
+//  Created by Abul Jaleel on 30/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ecommerce-swiftui/ecommerce_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerce_swiftuiApp.swift
+//  ecommerce-swiftui
+//
+//  Created by Abul Jaleel on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ecommerce_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
